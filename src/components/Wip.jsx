e1c0d4ae34e6e148fac6a38f4e5b81f7035f90bc
Joao Lopes <joaorfclopes@gmail.com>
2021-02-24 15:51:55 +0000

    Ellipsis animation added

--- src/components/Wip.jsx
+++ src/components/Wip.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import wip from "../assets/wip.svg";
+import wipgif from "../assets/wip.gif";
 
 export default function Wip() {
   return (
-    <object data={wip} type="image/svg+xml">
-      Wip
-    </object>
+    <div className="wip">
+      <h3>
+        <span>João Lopes</span>
+      </h3>
+      <object data={wip} type="image/svg+xml">
+        <img src={wipgif} alt="wip" />
+      </object>
+      <h2>
+        Website in development <span className="dot1">.</span>
+        <span className="dot2">.</span>
+        <span className="dot3">.</span>
+      </h2>
+    </div>
   );
 }
