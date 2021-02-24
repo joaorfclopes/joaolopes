824ef375bf3281c139620849abf276cf93085268
Joao Lopes <joaorfclopes@gmail.com>
2021-02-24 01:40:33 +0000

    wip svg added

---
+++ src/components/Wip.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import wip from "../assets/wip.svg";
+
+export default function Wip() {
+  return (
+    <object data={wip} type="image/svg+xml">
+      Wip
+    </object>
+  );
+}
