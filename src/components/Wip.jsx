6a23907d65aba70670d1afcff36128ea80c0e634
Joao Lopes <joaorfclopes@gmail.com>
2021-02-24 17:32:06 +0000

    WIP almost done

--- src/components/Wip.jsx
+++ src/components/Wip.jsx
@@ -1,17 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
+import $ from "jquery";
 import wip from "../assets/wip.svg";
 import wipgif from "../assets/wip.gif";
 
 export default function Wip() {
+  useEffect(() => {
+    setTimeout(() => {
+      $(".wip-text").addClass("show");
+    }, 200);
+  }, []);
+
   return (
     <div className="wip">
-      <h3>
+      <h3 className="wip-text wip-title hide">
         <span>João Lopes</span>
       </h3>
       <object data={wip} type="image/svg+xml">
         <img src={wipgif} alt="wip" />
       </object>
-      <h2>
+      <h2 className="wip-text wip-desc hide">
         Website in development <span className="dot1">.</span>
         <span className="dot2">.</span>
         <span className="dot3">.</span>
